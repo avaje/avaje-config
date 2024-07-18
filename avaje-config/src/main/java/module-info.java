1fa7327bdd804d351892a7261115e34d48c9980d
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-07-17 23:50:22 -0400

    static transitive

--- avaje-config/src/main/java/module-info.java
+++ avaje-config/src/main/java/module-info.java
@@ -2,11 +2,11 @@ module io.avaje.config {
 
   exports io.avaje.config;
 
-  requires transitive org.jspecify;
   requires transitive io.avaje.applog;
-  requires static org.yaml.snakeyaml;
 
   requires static io.avaje.spi;
+  requires static org.yaml.snakeyaml;
+  requires static transitive org.jspecify;
 
   uses io.avaje.config.ConfigExtension;
 
