905d233834f9085566c3c3bba1371a3910c5b0fc
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-10-13 17:42:08 -0400

    add utility for query parsing

--- avaje-config/src/main/java/io/avaje/config/URILoadContext.java
+++ avaje-config/src/main/java/io/avaje/config/URILoadContext.java
@@ -1,5 +1,11 @@
 package io.avaje.config;
 
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.UnaryOperator;
@@ -9,6 +15,23 @@ public interface URILoadContext {
   Optional<ConfigParser> configParser(String extension);
 
   Optional<String> getProperty(String key);
+
+  default Map<String, List<String>> splitQueryParams(URI uri) {
+    final Map<String, List<String>> queryPairs = new LinkedHashMap<>();
+    final String[] pairs = uri.getQuery().split("&");
+    for (String pair : pairs) {
+      final int idx = pair.indexOf("=");
+      final String key =
+          idx > 0 ? URLDecoder.decode(pair.substring(0, idx), StandardCharsets.UTF_8) : pair;
+
+      final String value =
+          idx > 0 && pair.length() > idx + 1
+              ? URLDecoder.decode(pair.substring(idx + 1), StandardCharsets.UTF_8)
+              : null;
+      queryPairs.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+    }
+    return queryPairs;
+  }
 }
 
 class DURILoadContext implements URILoadContext {
