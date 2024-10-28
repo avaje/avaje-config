e1987a935320a3c3eb95c29607d84c1895476309
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-10-28 19:30:25 -0400

    Update module-info.java

--- avaje-config-toml/src/main/java/module-info.java
+++ avaje-config-toml/src/main/java/module-info.java
@@ -1,13 +1,12 @@
-import io.avaje.config.ConfigExtension;
 import io.avaje.config.toml.TomlParser;
 
 module io.avaje.config.toml {
 
-  requires transitive io.avaje.config;
+  requires io.avaje.config;
   requires org.tomlj;
 
   exports io.avaje.config.toml;
 
-  provides ConfigExtension with TomlParser;
+  provides io.avaje.config.ConfigExtension with TomlParser;
 
 }
