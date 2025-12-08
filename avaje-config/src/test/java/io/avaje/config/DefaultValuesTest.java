abca01198875c2dfcaa04a914d70b7e3a17ea7dc
robin.bygrave <robin.bygrave@eroad.com>
2025-12-09 11:46:10 +1300

    Rename test to DefaultValuesTest

--- avaje-config/src/test/java/io/avaje/config/DefaultValuesTest.java
+++ avaje-config/src/test/java/io/avaje/config/DefaultValuesTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class DefaultFallbacksTest {
+public class DefaultValuesTest {
 
   @Test
   void toEnvKey() {
